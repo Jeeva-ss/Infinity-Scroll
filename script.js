ef41809cf0ca781b6764c0a740915003d4871e51
Jeeva <48169919+Jeeva-ss@users.noreply.github.com>
2023-03-06 12:31:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
 let photosArray = [];
 const count = 10;
 
-const apiKey1 = 'Xlb5NbESEpRlrNYH8YksLvlQ4e98fPc-o4ack27frPU';
-const apiKey2 = 'IH4MT-M7KA2QhjAGz9hzc6HufNP0KajW7H1_oVLQ980';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey1 ? apiKey1 : apiKey2}&count=${count}&query=${'nature'}&orientation=${'portrait'}`;
+const apiKey1 = "Xlb5NbESEpRlrNYH8YksLvlQ4e98fPc-o4ack27frPU";
+const apiKey2 = "IH4MT-M7KA2QhjAGz9hzc6HufNP0KajW7H1_oVLQ980";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey1 ? apiKey1 : apiKey2}&count=${count}&query=${"nature"}&orientation=${"portrait"}`;
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
 function displayPhotos() {
   photosArray.forEach((photo) => {
     // 'a' tag
-    const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: "photo.links.html",
+      target: "_blank",
+    });
     // 'img' tag
-    const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: "photo.urls.regular",
+      alt: "photo.alt_description",
+      title: "photo.alt_description",
+    });
     // nesting images inside anchor tag
     item.appendChild(img);
     // nesting anchor tag inside container
     imageContainer.appendChild(img);
-  })
+  });
 }
 
 const getPhotos = async () => {
@@ -32,9 +42,9 @@ const getPhotos = async () => {
     photosArray = await response.json();
     displayPhotos();
   } catch (error) {
-    imageContainer.innerHTML = `<h1>Comeback later...</h1>`
+    imageContainer.innerHTML = `<h1>Comeback later...</h1>`;
   }
-}
+};
 
 // On Load
 getPhotos();
