640b813f92093e9b59baa9204cdc39731ed0455d
Jeeva <48169919+Jeeva-ss@users.noreply.github.com>
2023-03-08 11:23:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const imageContainer = document.getElementById("image-container");
-const loader = document.getElementById("loader");
-let queryText = document.getElementById("queryText");
-const button = document.getElementById("button");
-const formContainer = document.getElementById("formContainer");
+// DOM
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
+// Variables
 let ready = false;
 let totalImages = 0;
 let imagesLoaded = 0;
 let photosArray = [];
 let count = 30;
-
 
 // Unsplash API calls
 const apiKey = `IH4MT-M7KA2QhjAGz9hzc6HufNP0KajW7H1_oVLQ980`;
@@ -36,23 +34,27 @@ function displayPhotos() {
   imagesLoaded = 0;
   totalImages = photosArray.length;
   photosArray.forEach((photo) => {
-    const item = document.createElement("a");
+    // creating 'a' tag
+    const item = document.createElement('a');
     setAttributes(item, {
       href: photo.links.html,
-      target: "_blank",
+      target: '_blank',
     });
-    const img = document.createElement("img");
+    // creating 'img' tag
+    const img = document.createElement('img');
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
-    img.addEventListener("load", imageLoaded);
+    // trigger loading event and update
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
 }
 
+// Aysnc function
 const getPhotos = async () => {
   try {
     const response = await fetch(apiUrl);
@@ -60,27 +62,16 @@ const getPhotos = async () => {
     displayPhotos();
   } catch (error) {
     imageContainer.innerHTML = `<h1>Comeback later...</h1>`;
-    imageContainer.classList.remove('image-container')
   }
 };
 
-// window.addEventListener("scroll", () => {
-//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
-//     ready = false;
-//     getPhotos();
-//   }
-// });
-
-// Searches for photos
-formContainer.addEventListener('submit', (e) => {
-  e.preventDefault()
-  console.log(queryText.value)
-  let x = queryText.value
-  apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=${x}&orientation=${`portrait`}`
-  getPhotos()
-  queryText.value = ''
-  imageContainer.innerHTML = ''
-})
+// Load event
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load
 getPhotos();
